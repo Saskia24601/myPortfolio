176a5d74e0e9d92b079057b61f1cdbccff8a04b8
Saskia24601 <sarah.l.robinson93@gmail.com>
2025-07-10 13:49:50 +0100

    Hero container added and layout improved.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let greeting = () => {
-  alert('Form has been submitted')
-  return aboutMe.html
+const words = [
+  " a Developer",
+  " a Mummy",
+  "a Baker",
+  "a Designer",
+  " a Wife",
+  "a Foodie",
+  "Driven",
+  "Hard Working",
+  "Dedicated"
+];
+const rotatingElement = document.getElementById("rotating-words");
+let index = 0;
+
+function rotateWords() {
+  // Fade out
+  rotatingElement.style.opacity = 0;
+
+  setTimeout(() => {
+    // Change text
+    index = (index + 1) % words.length;
+    rotatingElement.textContent = words[index];
+    // Fade in
+    rotatingElement.style.opacity = 1;
+  }, 500);
 }
+
+setInterval(rotateWords, 3000); // change word every 3 seconds
