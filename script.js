1ba5faf8792e00997368f0e38f294bc51729ae29
Saskia24601 <sarah.l.robinson93@gmail.com>
2025-07-14 23:59:00 +0100

    Updated portfolio styles

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.addEventListener("scroll", function () {
+  const header = this.document.getElementById("header");
+  if (this.window.scrollY > 750) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
 const words = [
   " a Developer",
   " a Mummy",
@@ -7,7 +16,7 @@ const words = [
   "a Foodie",
   "Driven",
   "Hard Working",
-  "Dedicated"
+  "Dedicated",
 ];
 const rotatingElement = document.getElementById("rotating-words");
 let index = 0;
